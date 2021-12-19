8eea9b1002f783a8a3faf6b90072f172c49e7a0d
mnhrwebsite <mhdnaufalhrtg@gmail.com>
2021-12-20 00:51:53 +0700

    second commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+$('.page-scroll').on('click', function (e) {
+  var tujuan = $(this).attr('href')
+  var elemetTujuan = $(tujuan)
+
+  $(html, body).animate(
+    {
+      scrollTop: elemetTujuan.offset().top - 50,
+    },
+    1500,
+  )
+  e.preventDefault
+})
+
 var owl = $('.owl-carousel')
 owl.owlCarousel({
   loop: true,
