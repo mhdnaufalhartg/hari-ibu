98ece7191f1eb638080908ff180cd19b0c7f8bca
mnhrwebsite <mhdnaufalhrtg@gmail.com>
2021-12-20 09:12:02 +0700

    + form contact

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
-$('.page-scroll').on('click', function (e) {
-  var tujuan = $(this).attr('href')
-  var elemetTujuan = $(tujuan)
+const scriptURL =
+  'https://script.google.com/macros/s/AKfycbw1hjfbyKhKIehqDpXyVNDAkT3katXNbO6vBCqx4JwPYAte0h9FQMPivrVWagMnEptMqw/exec'
+const form = document.forms['colleger-form-contact']
+const btnKirim = document.querySelector('.btn-kirim')
+const btnLoading = document.querySelector('.btn-loading')
+const myAlert = document.querySelector('.myAlert')
 
-  $(html, body).animate(
-    {
-      scrollTop: elemetTujuan.offset().top - 50,
-    },
-    1500,
-  )
-  e.preventDefault
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  btnLoading.classList.toggle('d-none')
+  btnKirim.classList.toggle('d-none')
+
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => {
+      btnLoading.classList.toggle('d-none')
+      btnKirim.classList.toggle('d-none')
+      myAlert.classList.toggle('d-none')
+      form.reset()
+      console.log('Success!', response)
+    })
+
+    .catch((error) => console.error('Error!', error.message))
 })
 
 var owl = $('.owl-carousel')
